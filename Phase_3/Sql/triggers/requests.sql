d19f7c92ca4f30544b83be51ad817484b7ee643e
mohammadali kakavand <malikakavand@gmail.com>
2022-02-07 11:01:43 -0500

    EDIT REQUEST TRIGGERS

--- Phase_3/Sql/triggers/requests.sql
+++ Phase_3/Sql/triggers/requests.sql
@@ -5,12 +5,12 @@ CREATE FUNCTION create_user_account()
    LANGUAGE PLPGSQL
 AS $$
 BEGIN
-   INSERT INTO Activation_req VALUES(NEW.user_id, CURRENT_TIMESTAMP, "pending", null);
+   INSERT INTO activation_req VALUES(NEW.user_id, CURRENT_TIMESTAMP, "pending", null);
 END;
 $$
 
 CREATE TRIGGER create_user_account_f
-   AFTER OF INSERT ON UserAccount
+   AFTER OF INSERT ON user_account
    FOR EACH ROW
        EXECUTE PROCEDURE create_user_account() 
 -----------------------------------------------------------
@@ -20,13 +20,13 @@ CREATE FUNCTION activate_account()
    LANGUAGE PLPGSQL
 AS $$
 BEGIN
-   IF (NEW.is_accepted = "accepted") THEN
-      UPDATE UserAccount SET is_accepted = "accepted" WHERE UserAccount.user_id = NEW.user_id;
+   IF (NEW.status = "accepted" and NEW.employee_number IS NOT NULL) THEN
+      UPDATE user_account SET is_active = TRUE WHERE user_account.user_id = NEW.user_id;
 END;
 $$
 
 CREATE TRIGGER activate_account_f
-   AFTER OF update ON PhysicalAccount
+   AFTER OF update ON activation_req
    FOR EACH ROW
        EXECUTE PROCEDURE activate_account() 
 -----------------------------------------------------------
@@ -37,8 +37,8 @@ CREATE FUNCTION create_bank_account()
 AS $$
 DECLARE account_num INT;
 BEGIN
-   IF (NEW.is_active = TRUE) THEN
-      INSERT INTO BankAccount VALUES(DEFAULT, NEW.user_id, NEW.balace, CURRENT_TIMESTAMP, true) RETURNING account_number INTO account_num;
+   IF (NEW.status = "accepted" and NEW.Employee_num IS NOT NULL) THEN
+      INSERT INTO BankAccount VALUES(DEFAULT, NEW.user_id, NEW.balace, CURRENT_TIMESTAMP, "accepted") RETURNING account_number INTO account_num;
       INSERT INTO SavingData VALUES(account_num, NEW.profit_percentage, NEW.type);
 END;
 $$
@@ -47,4 +47,29 @@ CREATE TRIGGER create_bank_account_f
    AFTER OF UPDATE ON bank_account_req
    FOR EACH ROW
        EXECUTE PROCEDURE create_bank_account() 
+-----------------------------------------------------------
+--create loan
+CREATE FUNCTION create_loan() 
+   RETURNS TRIGGER 
+   LANGUAGE PLPGSQL
+AS $$
+DECLARE loan_num INT;
+        AMOUNT_Instalment INT;
+BEGIN
+   IF (NEW.is_accepted = "accepted" and NEW.Manager_num IS NOT NULL) THEN
+      INSERT INTO Loan VALUES(DEFAULT, NEW.user_id, NEW.amount, "accepted", new.start_date, new.end_date, installment_number, new.profit_percentage, new.supervisor_id) RETURNING loan_number INTO loan_num;
+      
+      AMOUNT_Instalment = (NEW.amount * NEW.profit_percentage) / NEW.installment_number
+
+      for r in 1..new.installment_number loop
+      INSERT INTO Instalment VALUES(DEFAULT, loan_num, NEW.start_date + interval '1 month' * (r - 1), AMOUNT_Instalment "UNPAID", NULL);
+         end loop;
+
+END;
+$$
+
+CREATE TRIGGER create_loan_f
+   AFTER OF UPDATE ON loan_req
+   FOR EACH ROW
+       EXECUTE PROCEDURE create_loan()
 -----------------------------------------------------------
