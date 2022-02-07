3c7f80d3401f9e014b1206a397e798604857fe08
tandalalam <maziar56bnd@gmail.com>
2022-02-07 10:30:06 +0330

    First output from db create command line

---
+++ Phase_3/DB.py
@@ -0,0 +1,29 @@
+import psycopg2
+from tabulate import tabulate
+
+
+class DB:
+    HOST = '127.0.0.1'
+    PORT = 5432
+    user = 'admin'
+    database = 'db'
+    password = 'db-course'
+
+    def __init__(self):
+        self.connection = psycopg2.connect(database=DB.database, user=DB.user, host=DB.HOST, port=DB.PORT,
+                                           password=DB.password)
+        self.cursor = self.connection.cursor()
+
+    def get_output(self, query):
+        self.cursor.execute(query)
+        return self.cursor.fetchall(), [desc[0] for desc in self.cursor.description]
+
+    def see_all_bank_account(self, is_sort='FALSE'):
+        is_sort = True if is_sort.capitalize() == 'TRUE' else 'FALSE'
+        if is_sort:
+            query = 'SELECT * FROM bank_account NATURAL JOIN saving_data ORDER BY balance DESC;'
+        else:
+            query = 'SELECT * FROM NATURAL JOIN saving_data bank_account;'
+        data, header = self.get_output(query)
+        print(tabulate(data, headers=header))
+
--- Phase_3/UI.py
+++ Phase_3/UI.py
@@ -1,13 +1,46 @@
-import psycopg2
+from Phase_3 import DB
 
-HOST = '127.0.0.1'
-PORT = 5432
-user = 'admin'
-database = 'db'
-password = 'db-course'
 
-with psycopg2.connect(database=database, user=user, host=HOST, port=PORT, password=password) as connection:
-    cursor = connection.cursor()
-    cursor.execute("select * from test;")
-    data = cursor.fetchone()
-    print("query_result: ", data)
+class View:
+    def __init__(self, name, sub_views=[], function=None, father=None, explanation=''):
+        self.name = name
+        self.sub_views = sub_views
+        self.function = function
+        self.father = father
+        self.explanation = explanation
+
+    def show(self):
+        if self.function:
+            print(self.explanation)
+            command = input()
+            self.function(command)
+        else:
+            print(f'current: {self.name}')
+            print('------------------------')
+            i = 0
+            for i, subview in enumerate(self.sub_views):
+                print(f'{i}. {subview.name}.')
+            print(f'{i+1}. Exit.')
+            print('------------------------')
+            command = input()
+            if command != f'{i+1}':
+                self.execute(int(command))
+            else:
+                if self.father:
+                    self.father.show()
+
+    def execute(self, command):
+        view_to_show = self.sub_views[command]
+        view_to_show.show()
+
+    def add_view(self, view):
+        self.sub_views.append(view)
+
+
+if __name__ == '__main__':
+    db = DB.DB()
+    bank_account_view = View('Bank Accounts', function=db.see_all_bank_account,
+                             explanation='type TRUE to sort ordered by balance')
+    manager_view = View('Manager', sub_views=[bank_account_view])
+    manager_view.show()
+
